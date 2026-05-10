02fb0b67a6500d7d4e00d06448a94a1d72de161e
titilope <tmorolari@gmail.com>
2026-05-10 17:50:13 +0100

    upgrade the Jackson, assert, logback libs to fix dependency vulnerability

--- sharefile-spring-boot-starter/build.gradle.kts
+++ sharefile-spring-boot-starter/build.gradle.kts
@@ -2,12 +2,18 @@ plugins {
     id("org.springframework.boot") version "3.3.5" apply false
 }
 
+val jacksonVersion = "2.18.6"
+
 dependencies {
     api(project(":sharefile-sdk-client"))
     implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
+    implementation(platform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
     compileOnly(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
+    compileOnly(platform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
     annotationProcessor(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
+    annotationProcessor(platform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
     testImplementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
+    testImplementation(platform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
